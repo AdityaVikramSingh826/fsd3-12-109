d0414a202b30473f8f4befddfd21b76e3eb217f8
AdityaVikramSingh826 <adityavsingh26482.dpskalyanpur@gmail.com>
2026-07-30 12:16:34 +0530

    Added DOM lab files and updated hello.html

--- LAB-1/DOM5.js
+++ LAB-1/DOM5.js
@@ -1,29 +1,29 @@
 import { EventEmitter } from "events";
 class DomClass extends EventEmitter {
-    addEvenetListener(eventName, callback) {
-        this.on(eventName, callback);
-    }
-    removeEventListener(eventName, callback) {
-        this.off(eventName, callback);
-    }
-    dispatchEvent(eventName,eventData={}) {
-        const event = {
-            type: eventName,
-            timespam: new Date(),
-            ...eventData,
-        };
-        this.emit(eventName, event);
-    }
+  addEvenetListener(eventName, callback) {
+    this.on(eventName, callback);
+  }
+  removeEventListener(eventName, callback) {
+    this.off(eventName, callback);
+  }
+  dispatchEvent(eventName, eventData = {}) {
+    const event = {
+      type: eventName,
+      timespam: new Date(),
+      ...eventData,
+    };
+    this.emit(eventName, event);
+  }
 }
 const button = new DomClass();
 const handleClick = (event) => {
-    console.log(`Button clicked type: ${event.type} at ${event.timestamp}`);
+  console.log(`Button clicked type: ${event.type} at ${event.timestamp}`);
 };
 button.addEvenetListener("click", handleClick);
 button.dispatchEvent("click", {
-    target:"submitBtn",
+  target: "submitBtn",
 });
 button.removeEventListener("click", handleClick);
 button.dispatchEvent("click", {
-    target:"resetBtn",
+  target: "resetBtn",
 });
---
+++ LAB-2/Readme.md
@@ -0,0 +1,14 @@
+# EventLoop 
+
+JS is a synchronous and single threaded byfault 
+ 
+ ## there can be async behaviour 
+
+- with  browserAPI-
+setTimeout ,setInterval , setImmediate , nextTick<br>
+- with Promises<br>
+- with Event handlers
+
+### Important Topic 
+- promises
+- synchr and asynchr
