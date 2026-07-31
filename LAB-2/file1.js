d69f930b6d7adf87151b05991e061c461d9fddee
AdityaVikramSingh826 <adityavsingh26482.dpskalyanpur@gmail.com>
2026-07-31 15:55:31 +0530

    comm

--- LAB-2/file1.js
+++ LAB-2/file1.js
@@ -2,4 +2,4 @@ import { appendFile } from "fs/promises";
 import { writeFile } from "fs/promises";
 //await writeFile("hello.txt", "HELLO,WORLD!");
 
-await appendFile("hello.txt", "\nFS is much easy than others");
+await appendFile("hello.txt", "\nFS is much easier than others");
