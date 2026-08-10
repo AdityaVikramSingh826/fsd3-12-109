faede8c5d0f84b2049ac58303e6787e99546031b
AdityaVikramSingh826 <adityavsingh26482.dpskalyanpur@gmail.com>
2026-08-10 22:22:24 +0530

    commit

--- LAB-1/DOM.js
+++ LAB-1/DOM.js
@@ -2,6 +2,6 @@
 import { EventEmitter } from "events";
 const button = new EventEmitter();
 button.on("click", () => {
-    console.log("button clicked");
+    console.log("Button clicked");
 });
 button.emit("click")
