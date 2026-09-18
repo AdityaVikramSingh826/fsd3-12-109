6fa88cbf503c1e5d48e26442c8f7084ac7f33715
AdityaVikramSingh826 <adityavsingh26482.dpskalyanpur@gmail.com>
2026-09-18 14:48:25 +0530

    Added DOM lab files and updated hello.html

--- lab1/DOM.js
+++ lab1/DOM.js
@@ -2,6 +2,6 @@
 import { EventEmitter } from "events";
 const button = new EventEmitter();
 button.on("click", () => {
-    console.log("button clicked");
+    console.log("Button Clicked");
 });
 button.emit("click")
--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,12 +1,22 @@
 import http from "http";
-import { getUsers } from "./users.js";
+import { addUser, getUsers } from "./users.js";
 const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
   } else if ((req.url === "/api/users" && req.method === "POST")) {
-    res.end(JSON.stringify({ msg: "add user" }));
-  } else if ((req.url === "/api/users/1" && req.method === "GET")) {
-    res.end(JSON.stringify({ msg: "single user with id 1" }));
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    })
+    req.on('end', () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    })
+  } else if ((req.url.startsWith("/api/users/1") && req.method === "GET")) {
+    const userId = Number(req.url.split('/').pop())
+
+    res.end(JSON.stringify({ msg: `single user with id ${userId}` }));
   } else if ((req.url === "/api/users/1" && req.method === "PUT")) {
     res.end(JSON.stringify({ msg: "update user with id 1" }));
   } else if ((req.url === "/api/users/1" && req.method === "DELETE")) {
